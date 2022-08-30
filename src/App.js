9c8f3a866fb268d338d3e64171a746295fc133a5
stefaniavillella <99300919+stefaniavillella@users.noreply.github.com>
2022-08-29 21:25:57 -0300

    props

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <NavBar/>
-        <ItemListContainer/>
+        <ItemListContainer titulo="CATEGORIAS"/>
       </header>
     </div>
   );
